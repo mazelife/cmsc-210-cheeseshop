2fc53451b488e7ce804c605d3413335cdb3a31eb
James Stevenson <james.m.stevenson@gmail.com>
2022-04-25 22:02:06 -0400

    Pull SECRET_KEY from env var if set.

--- cheeseshop/settings.py
+++ cheeseshop/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "9&%l8tqlhr4ib=(-pa(-rtefotm6eow@4ll^*mpq86#gcq_wgo"
+SECRET_KEY = os.environ.get("SECRET_KEY", "9&%l8tqlhr4ib=(-pa(-rtefotm6eow@4ll^*mpq86#gcq_wgo")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
