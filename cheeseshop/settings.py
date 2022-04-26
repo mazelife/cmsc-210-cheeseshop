7514c6ed82457d90f70e893b90671642ad659693
James Stevenson <james.m.stevenson@gmail.com>
2022-04-25 21:58:25 -0400

    Switch DB config to use dj-database-url.

--- cheeseshop/settings.py
+++ cheeseshop/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import dj_database_url
 import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +76,8 @@ WSGI_APPLICATION = "cheeseshop.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(conn_max_age=500, default=f"sqlite:///{BASE_DIR}/db.sqlite3")
 }
 
 
